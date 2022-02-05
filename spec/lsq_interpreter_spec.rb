d21332fffe0be49a75689f5819f44050de3b117c
Steve Meyer <stephen.meyer@wisc.edu>
2022-02-05 10:44:37 -0600

    Tests for [lsq.interpreter] validation for axiom and rule l-strings

--- spec/lsq_interpreter_spec.rb
+++ spec/lsq_interpreter_spec.rb
@@ -44,7 +44,7 @@ RSpec.describe "L-Sequencer Interpreter" do
       end
 
       it "'l-string' is an alias for 'axiom'" do
-        send_message(client, "axiom F")
+        send_message(client, "l-string F")
         expect(@out1).to eq(["+", "+", "F", "Y"])
       end
     end
@@ -99,6 +99,90 @@ RSpec.describe "L-Sequencer Interpreter" do
       it "can keep advancing" do
         send_message(client, "advance")
         expect(@out1).to eq(["B", "C"])
+      end
+    end
+  end
+
+
+  context "when validation is enabled" do
+    before(:all) do
+      @patcher_sym  = random_symbol
+      open_mock("lsq.interpreter", @patcher_sym)
+      send_message(client, "strict 1")
+    end
+
+    after(:all) do
+      close_mock(@patcher_sym)
+    end
+
+    it "permits an axiom with no trailing metadata symbols" do
+      send_message(client, "axiom F + + Y + - X")
+      expected = [
+        "+", "+", "F", "Y", "+", "+", "-", "F", "[", "X", "]", "[", "-",
+        "Y", "+", "F", "]", "+", "-", "X", "[", "F", "+", "Y", "]"
+      ]
+      expect(@out1).to eq(expected)
+    end
+
+    it "permits adjacent branches" do
+      send_message(client, "l-string F [ + + Y ] [ + - X ]")
+      expected = [
+        "+", "+", "F", "Y", "[", "+", "+", "-", "F", "[", "X", "]", "[", "-", "Y",
+        "+", "F", "]", "]", "[", "+", "-", "X", "[", "F", "+", "Y", "]", "]"
+      ]
+      expect(@out1).to eq(expected)
+    end
+
+    it "does not permit adjacent open branch symbols" do
+      send_message(client, "axiom F [ [ X ] ]")
+      expect(@out1).to be_nil
+    end
+
+    it "does not permit unpaired metadata symbols" do
+      send_message(client, "axiom F + + Y + -")
+      expect(@out1).to be_nil
+    end
+
+    it "does not permit unpaired metadata symbols inside a branch" do
+      send_message(client, "axiom F [ + + Y ] [ + - ]")
+      expect(@out1).to be_nil
+    end
+
+    it "does not permit an axiom to close branches that were never opened" do
+      send_message(client, "axiom F [ ] ] X [")
+      expect(@out1).to be_nil
+    end
+
+    it "does not permit a rule to close branches that were never opened" do
+      send_message(client, "rule F ] X [")
+      send_message(client, "rules")
+      expect(@out2).to include(["F", "=>", "+", "+", "F", "Y"])
+      expect(@out2).not_to include(["F", "=>", "]", "X", "["])
+    end
+
+    it "does not permit unterminated branches" do
+      send_message(client, "rule F [ X ] [ Y")
+      send_message(client, "rules")
+      expect(@out2).to include(["F", "=>", "+", "+", "F", "Y"])
+      expect(@out2).not_to include(["F", "=>", "[", "X", "]", "[", "Y"])
+    end
+
+    context "when validation is turned off" do
+      before(:all) do
+        send_message(client, "strict 0")
+      end
+
+      it "allows an axiom that does not pass validation" do
+        send_message(client, "axiom F [ ] ] X [")
+        expected = ["+", "+", "F", "Y", "[", "]", "]", "X", "[", "F", "+", "Y", "]", "["]
+        expect(@out1).to eq(expected)
+      end
+
+      it "allows a rule that does not pass validation" do
+        send_message(client, "rule F [ X ] [ Y")
+        send_message(client, "rules")
+        expect(@out2).not_to include(["F", "=>", "+", "+", "F", "Y"])
+        expect(@out2).to include(["F", "=>", "[", "X", "]", "[", "Y"])
       end
     end
   end
